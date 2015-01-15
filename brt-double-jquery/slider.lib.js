070f7ea16e9ee5a0fe9fbd3eb80db9c563a49652
Gary Nakanelua <gnakan@gmail.com>
2015-01-14 17:45:03 -0800

    updated

---
+++ brt-double-jquery/slider.lib.js
@@ -0,0 +1,326 @@
+sliderJQ.easing.jswing = sliderJQ.easing.swing;
+sliderJQ.extend(sliderJQ.easing, {
+    def: "easeOutQuad",
+    swing: function(e, f, a, h, g) {
+        return sliderJQ.easing[sliderJQ.easing.def](e, f, a, h, g)
+    },
+    easeInQuad: function(e, f, a, h, g) {
+        return h * (f /= g) * f + a
+    },
+    easeOutQuad: function(e, f, a, h, g) {
+        return -h * (f /= g) * (f - 2) + a
+    },
+    easeInOutQuad: function(e, f, a, h, g) {
+        if ((f /= g / 2) < 1) {
+            return h / 2 * f * f + a
+        }
+        return -h / 2 * ((--f) * (f - 2) - 1) + a
+    },
+    easeInCubic: function(e, f, a, h, g) {
+        return h * (f /= g) * f * f + a
+    },
+    easeOutCubic: function(e, f, a, h, g) {
+        return h * ((f = f / g - 1) * f * f + 1) + a
+    },
+    easeInOutCubic: function(e, f, a, h, g) {
+        if ((f /= g / 2) < 1) {
+            return h / 2 * f * f * f + a
+        }
+        return h / 2 * ((f -= 2) * f * f + 2) + a
+    },
+    easeInQuart: function(e, f, a, h, g) {
+        return h * (f /= g) * f * f * f + a
+    },
+    easeOutQuart: function(e, f, a, h, g) {
+        return -h * ((f = f / g - 1) * f * f * f - 1) + a
+    },
+    easeInOutQuart: function(e, f, a, h, g) {
+        if ((f /= g / 2) < 1) {
+            return h / 2 * f * f * f * f + a
+        }
+        return -h / 2 * ((f -= 2) * f * f * f - 2) + a
+    },
+    easeInQuint: function(e, f, a, h, g) {
+        return h * (f /= g) * f * f * f * f + a
+    },
+    easeOutQuint: function(e, f, a, h, g) {
+        return h * ((f = f / g - 1) * f * f * f * f + 1) + a
+    },
+    easeInOutQuint: function(e, f, a, h, g) {
+        if ((f /= g / 2) < 1) {
+            return h / 2 * f * f * f * f * f + a
+        }
+        return h / 2 * ((f -= 2) * f * f * f * f + 2) + a
+    },
+    easeInSine: function(e, f, a, h, g) {
+        return -h * Math.cos(f / g * (Math.PI / 2)) + h + a
+    },
+    easeOutSine: function(e, f, a, h, g) {
+        return h * Math.sin(f / g * (Math.PI / 2)) + a
+    },
+    easeInOutSine: function(e, f, a, h, g) {
+        return -h / 2 * (Math.cos(Math.PI * f / g) - 1) + a
+    },
+    easeInExpo: function(e, f, a, h, g) {
+        return (f == 0) ? a : h * Math.pow(2, 10 * (f / g - 1)) + a
+    },
+    easeOutExpo: function(e, f, a, h, g) {
+        return (f == g) ? a + h : h * (-Math.pow(2, -10 * f / g) + 1) + a
+    },
+    easeInOutExpo: function(e, f, a, h, g) {
+        if (f == 0) {
+            return a
+        }
+        if (f == g) {
+            return a + h
+        }
+        if ((f /= g / 2) < 1) {
+            return h / 2 * Math.pow(2, 10 * (f - 1)) + a
+        }
+        return h / 2 * (-Math.pow(2, -10 * --f) + 2) + a
+    },
+    easeInCirc: function(e, f, a, h, g) {
+        return -h * (Math.sqrt(1 - (f /= g) * f) - 1) + a
+    },
+    easeOutCirc: function(e, f, a, h, g) {
+        return h * Math.sqrt(1 - (f = f / g - 1) * f) + a
+    },
+    easeInOutCirc: function(e, f, a, h, g) {
+        if ((f /= g / 2) < 1) {
+            return -h / 2 * (Math.sqrt(1 - f * f) - 1) + a
+        }
+        return h / 2 * (Math.sqrt(1 - (f -= 2) * f) + 1) + a
+    },
+    easeInElastic: function(f, h, e, l, k) {
+        var i = 1.70158;
+        var j = 0;
+        var g = l;
+        if (h == 0) {
+            return e
+        }
+        if ((h /= k) == 1) {
+            return e + l
+        }
+        if (!j) {
+            j = k * 0.3
+        }
+        if (g < Math.abs(l)) {
+            g = l;
+            var i = j / 4
+        } else {
+            var i = j / (2 * Math.PI) * Math.asin(l / g)
+        }
+        return -(g * Math.pow(2, 10 * (h -= 1)) * Math.sin((h * k - i) * (2 * Math.PI) / j)) + e
+    },
+    easeOutElastic: function(f, h, e, l, k) {
+        var i = 1.70158;
+        var j = 0;
+        var g = l;
+        if (h == 0) {
+            return e
+        }
+        if ((h /= k) == 1) {
+            return e + l
+        }
+        if (!j) {
+            j = k * 0.3
+        }
+        if (g < Math.abs(l)) {
+            g = l;
+            var i = j / 4
+        } else {
+            var i = j / (2 * Math.PI) * Math.asin(l / g)
+        }
+        return g * Math.pow(2, -10 * h) * Math.sin((h * k - i) * (2 * Math.PI) / j) + l + e
+    },
+    easeInOutElastic: function(f, h, e, l, k) {
+        var i = 1.70158;
+        var j = 0;
+        var g = l;
+        if (h == 0) {
+            return e
+        }
+        if ((h /= k / 2) == 2) {
+            return e + l
+        }
+        if (!j) {
+            j = k * (0.3 * 1.5)
+        }
+        if (g < Math.abs(l)) {
+            g = l;
+            var i = j / 4
+        } else {
+            var i = j / (2 * Math.PI) * Math.asin(l / g)
+        }
+        if (h < 1) {
+            return -0.5 * (g * Math.pow(2, 10 * (h -= 1)) * Math.sin((h * k - i) * (2 * Math.PI) / j)) + e
+        }
+        return g * Math.pow(2, -10 * (h -= 1)) * Math.sin((h * k - i) * (2 * Math.PI) / j) * 0.5 + l + e
+    },
+    easeInBack: function(e, f, a, i, h, g) {
+        if (g == undefined) {
+            g = 1.70158
+        }
+        return i * (f /= h) * f * ((g + 1) * f - g) + a
+    },
+    easeOutBack: function(e, f, a, i, h, g) {
+        if (g == undefined) {
+            g = 1.70158
+        }
+        return i * ((f = f / h - 1) * f * ((g + 1) * f + g) + 1) + a
+    },
+    easeInOutBack: function(e, f, a, i, h, g) {
+        if (g == undefined) {
+            g = 1.70158
+        }
+        if ((f /= h / 2) < 1) {
+            return i / 2 * (f * f * (((g *= (1.525)) + 1) * f - g)) + a
+        }
+        return i / 2 * ((f -= 2) * f * (((g *= (1.525)) + 1) * f + g) + 2) + a
+    },
+    easeInBounce: function(e, f, a, h, g) {
+        return h - sliderJQ.easing.easeOutBounce(e, g - f, 0, h, g) + a
+    },
+    easeOutBounce: function(e, f, a, h, g) {
+        if ((f /= g) < (1 / 2.75)) {
+            return h * (7.5625 * f * f) + a
+        } else {
+            if (f < (2 / 2.75)) {
+                return h * (7.5625 * (f -= (1.5 / 2.75)) * f + 0.75) + a
+            } else {
+                if (f < (2.5 / 2.75)) {
+                    return h * (7.5625 * (f -= (2.25 / 2.75)) * f + 0.9375) + a
+                } else {
+                    return h * (7.5625 * (f -= (2.625 / 2.75)) * f + 0.984375) + a
+                }
+            }
+        }
+    },
+    easeInOutBounce: function(e, f, a, h, g) {
+        if (f < g / 2) {
+            return sliderJQ.easing.easeInBounce(e, f * 2, 0, h, g) * 0.5 + a
+        }
+        return sliderJQ.easing.easeOutBounce(e, f * 2 - g, 0, h, g) * 0.5 + h * 0.5 + a
+    }
+});
+
+
+/*
+*
+*/
+
+
+/*!
+ * jQuery Cookie Plugin v1.4.0
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals.
+        factory(sliderJQ);
+    }
+}(function ($) {
+
+    var pluses = /\+/g;
+
+    function encode(s) {
+        return config.raw ? s : encodeURIComponent(s);
+    }
+
+    function decode(s) {
+        return config.raw ? s : decodeURIComponent(s);
+    }
+
+    function stringifyCookieValue(value) {
+        return encode(config.json ? JSON.stringify(value) : String(value));
+    }
+
+    function parseCookieValue(s) {
+        if (s.indexOf('"') === 0) {
+            // This is a quoted cookie as according to RFC2068, unescape...
+            s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+        }
+
+        try {
+            // Replace server-side written pluses with spaces.
+            // If we can't decode the cookie, ignore it, it's unusable.
+            // If we can't parse the cookie, ignore it, it's unusable.
+            s = decodeURIComponent(s.replace(pluses, ' '));
+            return config.json ? JSON.parse(s) : s;
+        } catch(e) {}
+    }
+
+    function read(s, converter) {
+        var value = config.raw ? s : parseCookieValue(s);
+        return $.isFunction(converter) ? converter(value) : value;
+    }
+
+    var config = $.cookie = function (key, value, options) {
+
+        // Write
+
+        if (value !== undefined && !$.isFunction(value)) {
+            options = $.extend({}, config.defaults, options);
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setTime(+t + days * 864e+5);
+            }
+
+            return (document.cookie = [
+                encode(key), '=', stringifyCookieValue(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path    ? '; path=' + options.path : '',
+                options.domain  ? '; domain=' + options.domain : '',
+                options.secure  ? '; secure' : ''
+            ].join(''));
+        }
+
+        // Read
+
+        var result = key ? undefined : {};
+
+        // To prevent the for loop in the first place assign an empty array
+        // in case there are no cookies at all. Also prevents odd result when
+        // calling $.cookie().
+        var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+        for (var i = 0, l = cookies.length; i < l; i++) {
+            var parts = cookies[i].split('=');
+            var name = decode(parts.shift());
+            var cookie = parts.join('=');
+
+            if (key && key === name) {
+                // If second argument (value) is a function it's a converter...
+                result = read(cookie, value);
+                break;
+            }
+
+            // Prevent storing a cookie that we couldn't decode.
+            if (!key && (cookie = read(cookie)) !== undefined) {
+                result[name] = cookie;
+            }
+        }
+
+        return result;
+    };
+
+    config.defaults = {};
+
+    $.removeCookie = function (key, options) {
+        if ($.cookie(key) === undefined) {
+            return false;
+        }
+
+        // Must not alter options, thus extending a fresh object...
+        $.cookie(key, '', $.extend({}, options, { expires: -1 }));
+        return !$.cookie(key);
+    };
+
+}));
--- brt-double-jquery/test.js
+++ brt-double-jquery/test.js
@@ -2,52 +2,54 @@ var thisNode = document.getElementsByTagName("script")[(document.getElementsByTa
 var src = thisNode.src;
 src = src.substring((window.location.protocol == "http:") ? 7 : 8, src.length).split("/")[0];
 var sliderOptions = {
-    version: "2.5",
+    version: "2.6",
     type: "noLogo",
     urls: {
         slider: src,
         protocol: location.protocol,
-        ad: "//optimizedby.brealtime.com/tt?id=3528110&size=160x600&referrer=listia.com",
-        css: "//cdn.cpxinteractive.com/slider/defs/3528110-918.css",
+        ad: "//optimizedby.brealtime.com/tt?id=1630773&size=120x600",
+        css: "//cdn.cpxinteractive.com/slider/defs/1630773-686.css",
         logo: "//cdn.cpxinteractive.com/slider/defs/solidblack.jpg"
     },
-    pid: 3528110,
-    trid: 0,
-    width: 160,
+    pid: 1630773,
+    width: 120,
     height: 600,
+    resize_width: undefined,
+    resize_height: null,
+    resize_ratio: null,
     debug: false,
-    id: 918,
+    id: 686,
+    referrer: "&",
     animationDelay: 0,
-    animationDuration: 0,
-    animationDirection: "top",
+    animationDuration: 500,
+    animationDirection: "left",
     animationEasing: "jswing",
-    toolbarPosition: "top-left",
-    horizontalPosition: "right",
-    verticalPosition: "top",
+    toolbarPosition: "right-top",
+    horizontalPosition: "left",
+    verticalPosition: "bottom",
     close: {
-        permanently: 1,
+        permanently: 0,
         wTab: 0
     },
     freqLimit: 0,
     freqLimitTimeRange: 24,
-    reverseFreqLimit: 0,
-    reverseFreqLimitTimeRange: 24,
     live: 1,
     scrollingDistance: 0,
-    displayOnMobile: 0,
-    minScreenWidth: 1355,
+    displayOnMobile: 1,
+    minScreenWidth: 0,
     margin: {
-        top: 238,
+        top: 0,
         right: 0,
-        bottom: 470,
-        left: 500
+        bottom: 0,
+        left: 0
     },
     position: {
         top: "0",
         right: "0",
         bottom: "0",
-        left: "50%"
-    }
+        left: "0"
+    },
+    minScreenWidth: 0
 };
 var SLIDER_JQ_VERSION = "1.9.1";
 if (needs_jquery()) {
@@ -80,21 +82,22 @@ function needs_jquery() {
 }
 
 function load_slider_libs() {
+    
     var script2 = document.createElement("script");
-    script2.innerHTML = "var sliderJQ = jQuery;jQuery.noConflict(true)";
+    script2.innerHTML = "var sliderJQ =jQuery.noConflict(true)";
     script2.type = "text/javascript";
     thisNode.parentNode.appendChild(script2);
 
-    cookie_lib = "//cdn.cpxinteractive.com/slider/lib/js/jquery.easing.cookie.min.js";
-    slider_options = "//cdn.cpxinteractive.com/slider/lib/js/app." + sliderOptions.version + ".js";
-    
-    sliderJQ(document).ready(function(){
-         sliderJQ.getScript(cookie_lib, function() {
-                sliderJQ.getScript(slider_options, function() {
-                    console.log('The slider is using jquery version ',sliderJQ().jquery);
-                    console.log('The main page is using jquery version ',jQuery().jquery);
-                })
-            
-            });
-    });
+    var libARR = ['slider.lib.js', "//cdn.cpxinteractive.com/slider/lib/js/app." + sliderOptions.version + ".js"];
+
+    for(i=0; i<libARR.length;i++){
+        var newScript = "script" + i;
+        newScript = document.createElement("script");
+        newScript.src = libARR[i];
+        thisNode.parentNode.appendChild(newScript);
+    }
+
+        console.log('The slider is using jquery version ',sliderJQ().jquery);
+        console.log('The main page is using jquery version ',jQuery().jquery);
+
 };

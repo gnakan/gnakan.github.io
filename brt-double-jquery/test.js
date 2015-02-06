6c3071e045f73671fa73b8581fb7aa0668d48cdd
Gary Nakanelua <gnakan@gmail.com>
2015-02-06 07:34:38 -0800

    updated

--- brt-double-jquery/test.js
+++ brt-double-jquery/test.js
@@ -1,94 +1,91 @@
 var thisNode = document.getElementsByTagName("script")[(document.getElementsByTagName("script").length - 1)];
 var src = thisNode.src;
 src = src.substring((window.location.protocol == "http:") ? 7 : 8, src.length).split("/")[0];
+var sliderJQ;
 var sliderOptions = {
-    version: "2.6",
+    version: "2.6.1",
     type: "noLogo",
     urls: {
         slider: src,
         protocol: location.protocol,
-        ad: "//optimizedby.brealtime.com/tt?id=1630773&size=120x600",
-        css: "//cdn.cpxinteractive.com/slider/defs/1630773-686.css",
+        ad: "//optimizedby.brealtime.com/tt?id=3528110&size=160x600&referrer=listia.com",
+        css: "//cdn.cpxinteractive.com/slider/defs/3528110-918.css",
         logo: "//cdn.cpxinteractive.com/slider/defs/solidblack.jpg"
     },
-    pid: 1630773,
-    width: 120,
+    pid: 3528110,
+    trid: 0,
+    width: 160,
     height: 600,
-    resize_width: undefined,
-    resize_height: null,
-    resize_ratio: null,
     debug: false,
-    id: 686,
-    referrer: "&",
+    id: 918,
     animationDelay: 0,
-    animationDuration: 500,
-    animationDirection: "left",
+    animationDuration: 0,
+    animationDirection: "top",
     animationEasing: "jswing",
-    toolbarPosition: "right-top",
-    horizontalPosition: "left",
-    verticalPosition: "bottom",
+    toolbarPosition: "top-left",
+    horizontalPosition: "right",
+    verticalPosition: "top",
     close: {
-        permanently: 0,
+        permanently: 1,
         wTab: 0
     },
     freqLimit: 0,
     freqLimitTimeRange: 24,
+    reverseFreqLimit: 0,
+    reverseFreqLimitTimeRange: 24,
     live: 1,
     scrollingDistance: 0,
-    displayOnMobile: 1,
-    minScreenWidth: 0,
+    displayOnMobile: 0,
+    minScreenWidth: 1355,
     margin: {
-        top: 0,
+        top: 238,
         right: 0,
-        bottom: 0,
-        left: 0
+        bottom: 470,
+        left: 500
     },
     position: {
         top: "0",
         right: "0",
         bottom: "0",
-        left: "0"
-    },
-    minScreenWidth: 0
+        left: "50%"
+    }
 };
 var SLIDER_JQ_VERSION = "1.9.1";
 if (needs_jquery()) {
-    console.log('jquery is needed');
     var script = document.createElement("script");
     script.src = "//cdnjs.cloudflare.com/ajax/libs/jquery/" + SLIDER_JQ_VERSION + "/jquery.min.js";
     script.type = "text/javascript";
     thisNode.parentNode.appendChild(script);
-    script.onload = function(){
-        load_slider_libs();
-    };
+    script.onload = function() {
+        load_slider_libs()
+    }
 } else {
-    load_slider_libs();
+    load_slider_libs()
 }
 
 function needs_jquery() {
     if (!window.jQuery) {
         return true
     }
-    var d = window.jQuery.fn.jquery.split(".");
-    var c = SLIDER_JQ_VERSION.split(".");
-    var b = [100000, 10000, 1000, 100, 10];
-    var a = 0;
-    var e = 0;
-    for (i in c) {
-        a += parseInt(d[i]) * b[i];
-        e += parseInt(c[i]) * b[i]
+    var b = window.jQuery.fn.jquery.split(".");
+    var a = SLIDER_JQ_VERSION.split(".");
+    var e = [100000, 10000, 1000, 100, 10];
+    var d = 0;
+    var c = 0;
+    for (i in a) {
+        d += parseInt(b[i]) * e[i];
+        c += parseInt(a[i]) * e[i]
     }
-    return (a < e)
+    return (d < c)
 }
 
 function load_slider_libs() {
-    
-    var script2 = document.createElement("script");
-    script2.innerHTML = "var sliderJQ =jQuery.noConflict(true)";
-    script2.type = "text/javascript";
-    thisNode.parentNode.appendChild(script2);
+    var noConflictScript = document.createElement("script");
+    noConflictScript.innerHTML = "var sliderJQ =jQuery.noConflict(true)";
+    noConflictScript.type = "text/javascript";
+    thisNode.parentNode.appendChild(noConflictScript);
 
-    var libARR = ['slider.lib.js', "//cdn.cpxinteractive.com/slider/lib/js/app." + sliderOptions.version + ".js"];
+    var libARR = ["//cdn.cpxinteractive.com/slider/lib/js/jquery.easing.cookie.min.v1.1.js", "//cdn.cpxinteractive.com/slider/lib/js/app." + sliderOptions.version + ".js"];
 
     for(i=0; i<libARR.length;i++){
         var newScript = "script" + i;
@@ -96,8 +93,4 @@ function load_slider_libs() {
         newScript.src = libARR[i];
         thisNode.parentNode.appendChild(newScript);
     }
-
-        console.log('The slider is using jquery version ',sliderJQ().jquery);
-        console.log('The main page is using jquery version ',jQuery().jquery);
-
 };

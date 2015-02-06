99d4266fcdfc3218c17b03686c5a929ca6818dc2
Gary Nakanelua <gnakan@gmail.com>
2015-02-06 07:35:43 -0800

    updated

--- brt-double-jquery/test.js
+++ brt-double-jquery/test.js
@@ -3,7 +3,7 @@ var src = thisNode.src;
 src = src.substring((window.location.protocol == "http:") ? 7 : 8, src.length).split("/")[0];
 var sliderJQ;
 var sliderOptions = {
-    version: "2.6.1",
+    version: "2.6",
     type: "noLogo",
     urls: {
         slider: src,

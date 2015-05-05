f07a8a5c84736bd48a45a6d8f05fee96aee9ce90
Gary Nakanelua <gnakan@gmail.com>
2015-05-04 17:59:26 -0700

    updated

--- bpcs-v3/assets/js/custom.js
+++ bpcs-v3/assets/js/custom.js
@@ -27,4 +27,18 @@ $(document).ready(function () {
         $(function () {
             $(".player").mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
         });
+
+
+
+
+    $('a[href^="#"]').on('click', function(event) {
+    var target = $(this).attr('href');
+    console.log(target);
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000);
+    }
+});
 });

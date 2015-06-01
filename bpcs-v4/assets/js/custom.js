6ca9b57ce6480e3478991c10c357b13c8bf0d137
Gary Nakanelua <gnakan@gmail.com>
2015-05-31 23:03:22 -0700

    updated

--- bpcs-v4/assets/js/custom.js
+++ bpcs-v4/assets/js/custom.js
@@ -1,5 +1,4 @@
-﻿
-/*=============================================================
+﻿/*=============================================================
     Authour URL: www.designbootstrap.com
 
     http://www.designbootstrap.com/
@@ -14,58 +13,64 @@
    
 ========================================================  */
 
-$(window).on('beforeunload', function(){
-  $(window).scrollTop(0);
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     // SCROLL SCRIPTS 
-        $('.scroll-me a').bind('click', function (event) { //just pass scroll-me class and start scrolling
+    $('.scroll-me a').bind('click', function(event) { //just pass scroll-me class and start scrolling
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1000, 'easeInOutQuad');
         event.preventDefault();
-        });
+    });
     // BACKGROUND VIDEO SCRIPTS
-        $(function () {
-            $(".player").mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
-        });
+    $(function() {
+        $(".player").mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
+    });
 
+    $(".read-more-btn").on('click', function(e) {
+        if ($(this).text() == "Read More") {
+            $(this).text("Show Less");
+            $(this).prev('.read-more').show(500);
+        } else {
+            $(this).text("Read More");
+            $(this).prev('.read-more').hide(500);
+        }
 
+    });
 
-
-        $(".read-more-btn").on('click', function(e){
-            if($(this).text() == "Read More"){
-                $(this).text("Show Less");
-                $(this).prev('.read-more').show(500);
-            }
-            else
-            {
-                $(this).text("Read More");
-                $(this).prev('.read-more').hide(500);
-            }
-            
-        })
-
-
-
-    $('.hero-control').on('click', function(){
+    $('.hero-control').on('click', function() {
         $('html, body').animate({
             scrollTop: window.innerHeight
         }, 1000);
     });
 
+    $('.share-link').on('click', function(e) {
+        e.preventDefault();
+        var menuWidth = 600;
+        
+        if ($(this).width() < menuWidth) 
+        {
+            $(this).addClass('share-link-show');
+        }
 
+    });
+
+    $('.show-link').on('click', function(e) {
+        $('div').removeClass('share-link-show')
+    });
 
     $('a[href^="#"]').on('click', function(event) {
-    var target = $(this).attr('href');
-    console.log(target);
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $(target).offset().top
-        }, 1000);
-    }
-});
+        var target = $(this).attr('href');
+        console.log(target);
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 1000);
+        }
+    });
 });

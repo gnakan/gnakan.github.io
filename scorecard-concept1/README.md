f47880331fcd0a7d6a6095288fb0105a48d0eac5
Gary Nakanelua <gnakan@gmail.com>
2015-09-28 12:45:55 -0700

    added new scorecard

---
+++ scorecard-concept1/README.md
@@ -0,0 +1,6 @@
+scorecard-concept-1
+==============
+
+Scorecard concept that uses a Google Spreadsheet as a source of data at https://docs.google.com/spreadsheets/d/1kc7OiV5Z3egmt4VjAs-TRgGScEPF_2lD4j02-FcnEss/pubhtml.
+
+
---
+++ scorecard-concept1/js/app.js
@@ -0,0 +1,106 @@
+$(document).ready(function() {
+    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1kc7OiV5Z3egmt4VjAs-TRgGScEPF_2lD4j02-FcnEss/pubhtml';
+    Tabletop.init({
+        key: public_spreadsheet_url,
+        callback: showInfo,
+        simpleSheet: false
+    })
+});
+
+
+var estimateData;
+var projectData;
+
+
+function showInfo(data, tabletop) {
+    //console.log(data);
+    estimateData = data;
+
+    createPageViewsChart()
+    populateTable();
+    renderProjectData();
+    console.log(estimateData);
+}
+
+function populateTable() {
+    $.each(estimateData, function(index, obj) {
+        if (obj['name'] == 'estimates') {
+            $.each(obj['elements'], function(i, o) {
+                var tableRow = "<tr>";
+                tableRow += "<td>" + o['*metrics'] + "</td>"
+                tableRow += "<td>" + o['jan'] + "</td>"
+                tableRow += "<td>" + o['feb'] + "</td>"
+                tableRow += "<td>" + o['mar'] + "</td>"
+                tableRow += "</tr>"
+                $('#estimated-table tbody').append(tableRow);
+            })
+
+        }
+    })
+}
+
+function renderProjectData() {
+    $.each(estimateData, function(index, obj) {
+        if (obj['name'] == 'overview') {
+            $('#project-title').html("Project: " + obj['elements']['0']['project-name']);
+            $('#project-site').html("<small>" + obj['elements']['0']['site'] + "</small>");
+        }
+    })
+}
+
+function createPageViewsChart() {
+    var estimateLabels = [];
+    var graphData = [];
+    var seriesData;
+
+    //get the labels
+    $.each(estimateData, function(index, obj) {
+        if (obj['name'] == 'estimates') {
+            $.each(obj['elements'], function(key, value) {
+                estimateLabels.push(value['*metrics']);
+            });
+
+            //get the data
+            $.each(obj['elements'], function(key, value) {
+                seriesData = []
+                    //skip the first value, write the next value to the first label array, write the second value to the second array
+                $.each(value, function(i, o) {
+                    if (i != undefined && i[0] != "*") {
+                        var noComma = o.replace(/[^\d\.\-\ ]/g, '');
+                        var dataPoint = parseFloat(noComma);
+                        seriesData.push(dataPoint);
+                    }
+                })
+                graphData.push(seriesData)
+            });
+        }
+    })
+
+    var data = {
+        // A labels array that can contain any sort of values
+        labels: estimateLabels,
+        // Our series array that contains series objects or in this case series data arrays
+        series: graphData
+    };
+
+    var options = {
+        showArea: true,
+          showLine: true,
+        showPoint: true,
+        fullWidth: true,
+        seriesBarDistance: 30,
+        axisX: {
+            showGrid: false,
+            showLabel: false
+        },
+        axisY: {
+             showLabel: false,
+            labelOffset: {
+                x: 20,
+                y: 0
+            },
+        }
+    };
+
+    new Chartist.Line('#page-views-chart', data, options);
+}

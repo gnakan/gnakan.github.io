6bee52d4f9c0c842e5bcca0f31fc71d234db4525
Gary Nakanelua <gnakan@gmail.com>
2015-12-04 16:26:19 -0800

    updated

---
+++ bpcs-v5/assets/js/custom.js
@@ -0,0 +1,127 @@
+﻿$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+$(document).ready(function() {
+    tableTopInit();
+
+    // SCROLL SCRIPTS 
+    $('.scroll-me a').bind('click', function(event) { //just pass scroll-me class and start scrolling
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutQuad');
+        event.preventDefault();
+    });
+
+
+    $(document).on('scroll', function() {
+
+        if ($(window).scrollTop() == 0) {
+            $('.bounce').show();
+        } else {
+            $('.bounce').hide();
+        }
+    });
+
+    $(".read-more-btn").on('click', function(e) {
+        if ($(this).text() == "Read More") {
+            $(this).text("Show Less");
+            $(this).prev('.read-more').show(500);
+        } else {
+            $(this).text("Read More");
+            $(this).prev('.read-more').hide(500);
+        }
+    });
+
+    $('.hero-control-scroll').on('click', function(e) {
+
+        $('html, body').animate({
+            scrollTop: window.innerHeight
+        }, 1000);
+    });
+
+    $('.arrow').on('click', function(e) {
+        $('html, body').animate({
+            scrollTop: window.innerHeight
+        }, 1000);
+    });
+
+    $('.share-link').on('click', function(e) {
+        e.preventDefault();
+        var menuWidth = 600;
+
+        if ($(this).width() < menuWidth) {
+            $(this).addClass('share-link-show');
+        }
+
+    });
+
+    $('.show-link').on('click', function(e) {
+        $('div').removeClass('share-link-show')
+    });
+
+
+    $('.award-location-1').on('click', function(e) {
+        window.open('http://www.bizjournals.com/seattle/morning_call/2015/07/blueprint-consulting-services-earned-eastside.html', "_blank");
+        window.location.href = './solutions.html#recognition';
+    });
+
+    $('.award-location-2').on('click', function(e) {
+        window.open('http://www.inc.com/profile/blueprint-consulting-services', "_blank");
+        window.location.href = './solutions.html#recognition';
+    });
+
+    $('.award-location-3').on('click', function(e) {
+        window.open('http://www.bizjournals.com/seattle/blog/techflash/2015/10/correction-blueprint-consulting-is-state-s.html', "_blank");
+        window.location.href = './solutions.html#recognition';
+    });
+
+
+
+
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this).attr('href');
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 1000);
+        }
+    });
+});
+
+
+function tableTopInit() {
+    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1nR4kcadW3rl8vhEFgLZJwmnsM8ZGGboWhXrDkyDpffM/pubhtml';
+    Tabletop.init({
+        key: public_spreadsheet_url,
+        callback: handleJobListingData,
+        simpleSheet: true
+    })
+};
+
+function handleJobListingData(data, tabletop) {
+    $("#job-listings").empty();
+
+    $.each(data, function(index, obj){
+        addJobListing(obj);
+    });
+};
+
+//creates the listing and appends it
+function addJobListing(obj){
+    var listingHTML = "<div class='col-md-4'>" +
+                    "<div class='white-paper-entry'> " + 
+                        "<div class='col-md-3'><i class='fa fa-globe  fa-4x'></i></div> " +
+                        "<div class='col-md-8'> " + 
+                            "<h4>" + obj['job-title'] + "</h4> " +
+                            "<p>" + obj['location'] + "</p>" +
+                            "<a href='" + obj['posting-url'] + "'>View job <i class='fa fa-arrow-right fa-fw'></i></a>" +  
+                       " </div>" +
+                    "</div> " +
+                "</div>";
+
+
+    $("#job-listings").append(listingHTML);
+}
---
+++ bpcs-v5/blog/app.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Blueprint Consulting Services | BLOG</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<meta name="description" content="Blueprint Consulting Services wants you to be part of the best industry talent, build your career, expand your experience and enjoy your work. Join Blueprint and architect your future!">
+
+<meta name="keywords" content="culture, careers at blueprint, talented employees, best consulting companies to work for, technology consulting, Bellevue, Seattle, Redmond, career opportunities, careers, community, values, career development, bellevue consulting jobs, redmond consulting jobs, washington consulting jobs">
+
+
+
+
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44141136-1', 'bpcs.com');
+  ga('send', 'pageview');
+
+</script>
+
+
+<script src="js/jquery-1.7.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.responsivemenu.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.elastislide.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/script.js"></script>
+<!--[if lt IE 8]>
+   <div style=' clear: both; text-align:center; position: relative;'>
+     <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+       <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+    </a>
+  </div>
+<![endif]-->
+    <!--[if lt IE 9]>
+   		<script src="js/html5.js"></script>
+  		<link rel="stylesheet" href="css/ie.css"> 
+	<![endif]-->
+</head>
+<body>
+<!-- header -->
+<div class="bg-header">
+<div class="pad-main"></div>
+	<div class="marg">
+		<header>
+			<div class="container_12">
+				<div class="grid_12 alpha omega">
+					<center><h1 class="logo"><a href="index.html"><center><img src="images/logo.png" alt="blueprint consulting services"></center></a></h1></center>
+					<nav id="main-menu">
+						<ul class="sf-menu responsive-menu">
+							<li><a href="about.html">about</a></li>
+							</ul>
+							
+							<ul class="sf-menuu responsive-menu">
+							
+							<li><a href="services.html">services</a>
+							</ul>
+                                    
+                            <ul class="sf-menuuu responsive-menu">
+							
+                            <li><a href="clients.html">clients</a></li>
+                            
+                            </ul>
+                            
+                             <ul class="sf-menuuuuu responsive-menu">
+							<li><a href="team.html">Our Team</a></li>
+						</ul>
+                            
+                            <ul class="sf-menuuuu responsive-menu">
+						<li><a href="http://jobs-bpcs.icims.com">careers</a></li>
+							</ul>
+							 <ul class="sf-menuuuuu responsive-menu">
+							<li><a href="contact.html">contact</a></li>
+						</ul>
+						<div class="clear"></div>
+					</nav>
+					<div class="wrapper">
+					<div class="grid_12 alpha omega">
+							<center><figure class="imgindent3"><a href="blog.html"><img src="images/blog.jpg" width="960" height="140" alt=""/></a></figure>
+					</center>
+					</div>
+					</div>
+						
+					<div class="clear"></div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</header>
+	</div>
+</div>
+<!-- content -->
+<div class="bg">
+	<div class="bg2">
+		<section id="content" name="content"> 
+			<div class="container_12 padcontent2">
+				<div class="wrapper">
+					<div class="grid_8 alpha">
+						<br />
+						<div class="wrapper">
+							<div class="extra-wrap2">
+                            
+                            
+                            
+ <!-- image                          
+						<div class="imgindent">
+							<figure><img style="padding-left:5px; padding-right:8px;float:left;" height="40%" width="40%" src="images/blogoctober.jpg" alt="blueprint consulting services"></figure></div>
+                            
+-->	
+							
+						
+							
+<div class="teamwrap1">
+<center><h1 style="padding-top:20px; color:white;"> </h1> <h4 style="padding-bottom:8px;">Do you need an app for that? - January 2015</h4></center>
+                          
+	
+				
+					
+							
+<p style="margin-top:10px; padding:5px"><i>..your scientists were so preoccupied with whether or not they could that they didn't stop to think if they should.</i> — Dr. Ian Malcolm (Jurassic Park)</p>
+
+<p style="margin-top:10px; padding:5px"><b><u>Humble Beginnings</u></b></p>
+
+<p style="margin-top:10px; padding:5px">
+Recently, I was in the good company of a group of friends and acquaintances, enjoying some food, wine, and cigars. Towards the end of the evening, conversations shifted from sports, current events, religion and politics to business and technology by a couple of guys who were co-founders of a winery startup.</p>
+
+<p style="margin-top:10px; padding:5px">
+I absolutely love talking with passionate entrepreneurs. These two particular co-founders were wine aficionados and fervent believers in the power of wine to build and foster personal relationships. I was surrounded by the fruits of their labor: oak wine barrels filled with aging reds and well-organized micro-oxygenation equipment, all contained within a garage. It was an impressive labor of love.</p> 
+
+<p style="margin-top:10px; padding:5px">
+As we sat around, reviewing various digital artifacts, such as the winery’s logo and concepts for the wine bottle labels, the question was posed:</p>
+
+<p style="margin-top:10px; padding:5px">“What do you think about making a mobile app for the winery?”</p>
+
+<p style="margin-top:10px; padding:5px"><b><u>Do people actually use apps?</u></b></p>
+
+<p style="margin-top:10px; padding:5px">
+Infographics (like the one below) really get people excited about the idea of creating a mobile app:</p>
+<br>
+
+
+ <!-- image -->
+
+<figure><img style="padding-left:5px; padding-right:8px; padding-bottom: 8px; float:left;" height="100%" width="100%" src="images/graph.jpg" alt="blueprint consulting services"></figure>
+
+
+<p style="margin-top:10px; padding:5px"><i>Infographic provided by Statista</i></p>
+
+<p style="margin-top:10px; padding:5px">
+179 million app downloads in 2014, with a projected 49% increase by 2017 is enough to spark the idea of creating a mobile app for almost any reason, especially when mobile app usage in 2014 increased by 86%.</p>
+
+<p style="margin-top:10px; padding:5px">
+Another interesting statistic is provided by Nielsen:</p>
+<br>
+
+
+
+ <!-- image -->
+
+<figure><img style="padding-left:5px; padding-right:8px; padding-bottom:20px; float:left;" height="100%" width="100%" src="images/graph2.png" alt="blueprint consulting services"></figure>
+
+
+<p style="margin-top:10px; padding:5px">
+Not only is the number of apps downloaded increasing, but the time people spend using mobile apps is increasing as well. But, if you look at the numbers a bit more, there is another trend: While app usage is on the rise, the average number of apps used is relatively unchanged.</p>
+
+<p style="margin-top:10px; padding:5px">
+Yes, people use apps. However, your potential app is competing with the other 25 (on average) apps people use.</p>
+
+<p style="margin-top:10px; padding:5px">
+<b>To stand out, your potential app has to be great.</b></p>
+
+<p style="margin-top:10px; padding:5px"><b><u>What makes a great app?</u></b></p>
+
+<p style="margin-top:10px; padding:5px">
+As a success metric, the word “great” can be highly subjective.</p>
+
+<p style="margin-top:10px; padding:5px">
+To management, a great app may be one that has 12,000 monthly downloads, 5 million monthly pageviews, and $20k in monthly advertising revenues.</p>
+
+<p style="margin-top:10px; padding:5px">
+To a developer, a great app may be an AngularJS web app, that uses Node.js, MongoDB and socket.io for delivering real-time sports information from a third party API, with a performance improvement of 62.7 seconds per request over the previous iteration.</p>
+
+<p style="margin-top:10px; padding:5px">
+However, to the manager, developer and designer (roles I have held in various organizations throughout my career), I would argue that these are not descriptions of a great app.</p>
+
+<p style="margin-top:10px; padding:5px">
+Revenue is the result of a valid business model. Development libraries, frameworks and technology selections are the result of a thorough development analysis. Performance improvement is the response to internal testing and user feedback. Visual designs are the result of usability tests and/or stakeholder input. However, by themselves, they are merely facets of a great app.</p>
+
+<p style="margin-top:10px; padding:5px">
+<b>A great app is one that its core audience loves to use, regardless of competition or trend.</b></p>
+
+<p style="margin-top:10px; padding:5px"><b><u>Who is your core audience?</u></b></p>
+
+<p style="margin-top:10px; padding:5px">
+The terms “core audience” and “target market” are often, yet inappropriately, used interchangeably.</p>
+
+<p style="margin-top:10px; padding:5px">
+Philip Kotler, a well known and respected marketing author, consultant and professor is often quoted for defining target market as <i>“a well-defined set of customers whose needs the organization plans to satisfy”.</i> Marketers will often structure this set of customers based upon demographics such as age, gender, occupation, education level, marital status, income, and location. Other management types may talk about target markets in terms of segment attractiveness, and whether that segment will fit into a company’s overall business objectives, available resources and capabilities.</p>
+
+<p style="margin-top:10px; padding:5px">
+Focusing on the target market is how an app will be monetized, but does not guarantee a great app. In 2008, an iPhone app named “iFart Mobile” reached the #1 spot on the Top 25 Top Paid chart within the Apple App Store and was earning an estimated $10,000 a day. The developer, Joel Comm, understood his target market and profited accordingly, releasing a simple and funny novelty app that people were willing to pay 99 cents for.</p>
+
+<p style="margin-top:10px; padding:5px">
+Is iFart Mobile a great app? Despite it’s early financial success with its target market, it’s hard to determine who the core audience is. A search on the Apple App store returns over 1,000 apps with similar functionality. Overall, the app has an average rating with a mix bag of reviews ranging from “OMG so funny” to “Bad App”.</p>
+
+<p style="margin-top:10px; padding:5px">
+An app’s core audience will evangelize the app to their family, friends and any stranger who is willing to listen. They will demonstrate, illustrate and pontificate all the ways the app has changed their lives. An app’s core audience will find other uses for the app, often pushing the use case beyond the original intent. They do all of this, not because of clever marketing, pricing or popularity, but because the app addressed a very specific pain in the core audience’s life in an innovative manner.</p>
+
+<p style="margin-top:10px; padding:5px">
+<b>An app’s core audience is defined by a very particular problem.</b></p>
+
+<p style="margin-top:10px; padding:5px"><b><u>What problem does your app address?</u></b></p>
+
+<p style="margin-top:10px; padding:5px">
+<i>You have to be burning with an idea, or a problem, or a wrong that you want to right.</i> — Steve Jobs</p>
+
+<p style="margin-top:10px; padding:5px">
+The best problem to address is one you have direct experience with right now. This sounds easy, but hubris may cloud a potential product owner’s judgement; they are more in love with being a product owner than solving an actual problem.</p>
+
+<p style="margin-top:10px; padding:5px">
+Some product owners will define the problem as “We don't have an app”. As a result, they often end up with a “Me too” app with low usage, lackluster adoption rate and will accept the “It’s good enough” mentality when it comes to measuring the app’s success.</p>
+
+<p style="margin-top:10px; padding:5px">
+Other product owners will confuse a problem with an observation. They will evaluate perceived “holes” in a particular market, making statements such as “Over X number of people perform action Y, yet no app exists that allows them to do Z”. The lack of an app doesn't necessarily mean a problem exists.</p>
+
+<p style="margin-top:10px; padding:5px">
+Then, there are those product owners that lack a solid understanding of how their core audience uses technology, yet, forge a problem that has no real pain point. You may hear statements such as “Our content gets X number of page views from users on a mobile device, so we should build an app that displays our content natively. This will be a better user experience”. This may sound similar to the product owner that wants a “Me too” app, but their motivations are very different. However, neither have defined a real problem.</p>
+
+<p style="margin-top:10px; padding:5px"><b><u>The logical existence of an app defined</u></b></p>
+
+<p style="margin-top:10px; padding:5px">
+Based upon our conclusions, we can apply the following deductive reasoning:</p>
+
+<p style="margin-top:10px; padding:5px">
+A core audience is defined by a real problem.
+<br>An app cannot exist without a core audience.
+<br>Therefore, an app cannot be defined without a real problem.</p>
+
+<p style="margin-top:10px; padding:5px">
+Considering this logic, the true question a potential product owner should ask is not “Do you need an app for that” but rather, “Do you have a real problem you wish to solve”?</p>
+
+<p style="margin-top:10px; padding:5px"><b><u>What about the winery?</u></b></p>
+
+<p style="margin-top:10px; padding:5px">
+One final question remains: What do I think about making a mobile app for the winery?</p>
+
+<p style="margin-top:10px; padding:5px">
+When first posed the question, I carried on for a good 10 minutes without directly answering the question. Instead, I opted to provoke the co-founders to think about the need for a mobile app differently. After said provocation, one of the cofounders noted, “Perhaps the app transcends the winery”.</p>
+
+<p style="margin-top:10px; padding:5px">
+Exactly.</p>
+
+<p style="margin-top:10px; padding:5px">
+Framing the question around my own experiences, I want an app that helps me make sense of purchasing local wines. I purchase wines based upon my limited perception in how they will pair with the food I intend to eat. I understand the basics; red wines should be had with red meat and white wines with white meat. I have books specific to wine appreciation on my bookshelf. I have even gleaned some specific bits of knowledge from wine makers over the years, such as pairing a good syrah with steak due in part to the peppery note in the finish.</p>
+
+<p style="margin-top:10px; padding:5px">
+I'm new to the area and know just enough about wine to be dangerous. I’m intrigued by labels, listen to recommendations and appreciate the passion shared by local wine drinkers. My pain point: what local wines should I purchase?</p>
+
+<p style="margin-top:10px; padding:5px">
+Give me an app that recommends wine pairings based upon the simple question “What are you eating tonight”. I want the recommended wines to come from a local winery, with reviews provided by residents and local app users. Give me tips about the best way to store, open and serve the wine. Give me a breakdown of the best local places to purchase wines. Give me recipes for food dishes that, when paired with the recommended wines, bring out the best flavors in both the food and drink. In short, aid me in my aspirations to be a man that truly understands how to enjoy good food, wine and company.</p>
+
+<p style="margin-top:10px; padding:5px">
+Do those things, and I will use your app constantly. Not because I have to, but because I love to use great apps.</p>
+
+
+<p style="margin-top:10px; padding:5px"><b>- Gary Nakanelua, Developer
+<br>Blueprint Consulting Services</b></p>
+
+
+
+							
+						</div>
+						
+						
+                 
+	
+							</div>
+                            
+                                              <br><Br>
+                            <!-- begin htmlcommentbox.com -->
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Add your comments here</a></div>
+ <link rel="stylesheet" type="text/css" href="http://www.htmlcommentbox.com/static/skins/default/skin.css" />
+ <script type="text/javascript" language="javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={  };} (function(){s=document.createElement("script");s.setAttribute("type","text/javascript");s.setAttribute("src", "http://www.htmlcommentbox.com/jread?page="+escape((window.hcb_user && hcb_user.PAGE)||(""+window.location)).replace("+","%2B")+"&opts=470&num=10");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+<!-- end htmlcommentbox.com -->
+
+
+							<div class="extra-wrap">
+							
+							</div>
+						</div>
+					</div>
+					<br />
+					<div class="grid_4 omega">
+					<?php include '/includes/posts.html'; ?>
+				<div class="imgindent">
+                 <figure><a href="mailto:careers@bpcs.com" target="_blank"><img src="images/email_blueprint.jpg" alt="blueprint consulting services"></a></figure>
+                 </div>
+				</div>
+               
+				
+				
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+<!-- footer -->
+<footer>
+	<div class="container_12">
+		<div class="z1">
+			<div class="grid_3">
+				<h1><a href="index.html"><img src="images/logo-footer.png" alt="Blueprint Consulting"></a></h1>
+			</div>
+			<div class="grid_6">
+			<div class="imgindent">
+				<center><img src="images/footerpartner.png" alt="Blueprint Consulting"></center>
+				</div>
+			</div>
+			<div class="grid_3">
+				<ul class="tooltips">
+					<li><a href="http://www.linkedin.com/company/blueprint-consulting-services" class="tooltip" target="blank"><strong><img src="images/icon3.png" alt="blueprint consulting services linkedin"></strong><span>Linkedin</span></a></li>
+                    
+					<li><a href="https://twitter.com/BlueprintCSLLC" class="tooltip" target="blank"><strong><img src="images/icon2.png" alt="blueprint consulting services twitter"></strong><span>Twitter</span></a></li>
+                    
+					<li><a href="https://www.facebook.com/pages/Blueprint-Consulting-Services-LLC/566844073359774" class="tooltip"target="blank"><strong><img src="images/icon4.png" alt="blueprint consulting services facebook"></strong><span>Facebook</span></a></li>
+					<li><a href="http://bpcs.com/blog.html" class="tooltip"target="blank"><strong><img src="images/icon5.png" alt="blueprint consulting services blog"></strong><span>Blog</span></a></li></ul>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="z2"><div class="grid_3"><div class="copyright">&copy; 2014 Blueprint Consulting Services</div></div><div class="clear"></div></div>
+	</div>
+</footer>     
+</body>
+</html>

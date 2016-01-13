67f2d6c0d45eea5a52844d26f2e0a9cf6557875e
Gary Nakanelua <gnakan@gmail.com>
2016-01-13 14:12:21 -0800

    updated

--- bpcs-v4/assets/js/greenhouse.js
+++ bpcs-v4/assets/js/greenhouse.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-
-
   $.get("https://api.greenhouse.io/v1/boards/bpcs/embed/jobs", function(data) {
     $('#jobListings').empty();
     $.each(data.jobs, function(index, obj) {
@@ -8,7 +6,6 @@ $(document).ready(function() {
       jobTemplate += "<div class='col-md-8'><h4>" + obj.title + "</h4><p>" + obj.location.name + "</p>";
       jobTemplate += "<a href='" + obj.absolute_url + "' target='_blank'>View job <i class='fa fa-arrow-right fa-fw'></i></a>";
       jobTemplate += "</div></div></div>";
-      console.log(obj.title);
       $('#jobListings').append(jobTemplate);
     });
   });

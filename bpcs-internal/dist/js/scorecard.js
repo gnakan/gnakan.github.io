793d12487b896d20f1bf643f6952dd25697293aa
Gary Nakanelua <gnakan@gmail.com>
2016-02-23 21:51:08 -0500

    updated

---
+++ bpcs-internal/dist/js/scorecard.js
@@ -0,0 +1,113 @@
+var selected;
+var scorecardData;
+var currentMonth = "January 2016";
+
+$(document).ready(function() {
+  getTableData();
+
+  $('.sidebar-menu').on('click', 'li', function() {
+    selected = $(this).data('name');
+    $('#scorecardTitle').html(selected);
+
+    loadMetrics();
+  });
+});
+
+
+
+function getTableData() {
+  Tabletop.init({
+    key: '1EYgL1FVds4UUzQC1k8PHcpUKvTWAo8Rc9NF8j8qZuFM',
+    callback: buildSidebar,
+    simpleSheet: false
+  });
+}
+
+function buildSidebar(data) {
+  scorecardData = data;
+  $('.sidebar-menu').empty();
+  $('.sidebar-menu').append();
+
+  $.each(data, function(index, obj) {
+    if (obj.name !== "selections")
+      $('.sidebar-menu').append("<li class='dashboardLink' data-name='" + obj.name + "'><a href='#'><i class='fa fa-dashboard'></i><span>" + obj.name + "</span></a></li>");
+  });
+
+  var $links = $('.dashboardLink');
+  var alphabeticallyOrderedDivs = $links.sort(function(a, b) {
+    return $(a).attr('data-name') > $(b).attr('data-name');
+  });
+
+  $('.sidebar-menu').html("<li class='header'>PEOPLE</li>").append(alphabeticallyOrderedDivs);
+}
+
+function sorter(a, b) {
+  return $(a).attr('data-name') > $(b).attr('data-name');
+}
+
+function loadMetrics(data) {
+  $('.metrics').empty();
+  $.each(scorecardData, function(index, obj) {
+    if (obj.name === selected) {
+      $.each(obj.elements, function(ind, obj) {
+        var goal, actual, after;
+        var status = 'blue';
+        if (obj.after !== "%") {
+          goal = obj.before + obj['jan-g'];
+          actual = obj.before + obj['jan-a'];
+          after = obj.after;
+        } else {
+          goal = obj.before + obj['jan-g'] + obj.after;
+          actual = obj.before + obj['jan-a'] + obj.after;
+          after = "";
+        }
+
+        if (obj.status !== "") {
+          status = obj.status;
+        }
+
+        addMetric(obj.metric, goal, actual, after, obj.icon, status, obj.tooltip, '.metrics');
+      });
+    }
+
+  });
+}
+
+
+
+function addMetric(name, goal, actual, after, icon, status, tooltip, div) {
+
+  var source = $('#headline1').html();
+  var template = Handlebars.compile(source);
+  var data = {
+    name: name,
+    goal: goal,
+    actual: actual,
+    after: after,
+    icon: icon,
+    status: status,
+    currentMonth: currentMonth,
+    tooltip: tooltip
+  };
+
+  var template1 = "<div class='col-lg-6'>" +
+    "<div class='box box-info'>" +
+    "<div class='box-header'>" +
+    "<i class='fa fa-line-chart'></i>" +
+    "<h3 class='box-title'>" + name + "</h3>" +
+    "<p><small class='text-muted'>For January 2016</small></p></div>" +
+    "<div class='box-body'>" +
+    "<div class='row'>" +
+    "<div class='col-md-3'>" +
+    "<div class='row headline-info'>" +
+    "<h3 class='text-primary'>" + goal + "</h3>" +
+    "<p>Goal</p>" +
+    "<p>100</p></div>" +
+    "<div class='row headline-info'><h3 style='color: rgba(0,0,0,.5);'>" + actual + "</h3>" +
+    "<p>Actual</p><p>93</p></div></div>" +
+    "<div class='col-md-9'><canvas id='' width='700' height='250'></canvas></div></div>" +
+    "</div></div></div>";
+
+  $(div).append(template(data));
+
+}
